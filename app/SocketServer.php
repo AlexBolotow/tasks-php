b7c0b87191717a6160d2ef89984a2830ede9da69
AlexBolotow <bolotow27@gmail.com>
2025-03-07 13:31:54 +0300

    added send file to server functionality

--- app/SocketServer.php
+++ app/SocketServer.php
@@ -20,11 +20,43 @@ class SocketServer
         socket_bind($this->socket, $this->host, $this->port );
         socket_listen($this->socket);
 
-        $connect = socket_accept($this->socket);
-        $message = socket_read($connect, 1024);
-        echo $message;
+        if ($connect = socket_accept($this->socket)) {
+            if ($this->acceptTextFile($connect)) {
+                socket_write($connect, 'File accepted successful');
+            } else {
+                socket_write($connect, 'File not accepted');
+            }
+        }
 
-        socket_write($connect, "message received by server");
+        //socket_close($this->socket);
+    }
+
+    private function acceptTextFile(\Socket $connect): bool {
+        socket_write($connect, "Hello from server. In first message send name of file (****.txt).
+        Then send file content. End of work - message contains only empty line\n");
+
+        $fileName = socket_read($connect, 1024);
+        if (!str_ends_with($fileName, '.txt')) {
+            socket_write($connect, "Wrong file name\n");
+            return false;
+        } else {
+            socket_write($connect, "File created\n");
+        }
+
+        $path = '/home/alex/PhpstormProjects/tasks-php/resources/' . $fileName;
+        $file = fopen($path, 'w');
+        $message = socket_read($connect, 1024);
+
+        while (!empty($message)) {
+            //echo '1';
+            fwrite($file, $message);
+            socket_write($connect, "Message accepted\n");
+            $message = socket_read($connect, 1024);
+        }
+
+        fclose($file);
+
+        return true;
     }
 
     public function __destruct() {
--- public/client.php
+++ public/client.php
@@ -7,8 +7,20 @@ require_once("../vendor/autoload.php");
 $host = "127.0.0.1";
 $port = 8080;
 
-$socketServerClient = new SocketServerClient($host, $port);
+//TODO попробовать параллельную работу
+for ($i = 0; $i < 3; $i++) {
 
-$socketServerClient->connect();
-$socketServerClient->sendMessage("hello from client");
+    $socketServerClient = new SocketServerClient($host, $port);
 
+    $socketServerClient->connect();
+
+    $filename = 'client.txt';
+
+    $socketServerClient->sendMessage($filename);
+    $socketServerClient->sendMessage("hello from client\n");
+    $socketServerClient->sendMessage("its my first message\n");
+    $socketServerClient->sendMessage("its my second message\n");
+    $socketServerClient->sendMessage("its my third message\n");
+    $socketServerClient->sendMessage("its my fourth message\n");
+    $socketServerClient->sendMessage("");
+}
